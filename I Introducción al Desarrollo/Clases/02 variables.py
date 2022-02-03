d5fbdd23d87aebbba3a51708f5829b5aaa87fdb2
Carlos Dubón <61991110+celtaced@users.noreply.github.com>
2022-02-03 17:15:06 -0600

    Primeros Archivos
    Se colocan los primeros archivos

---
+++ I Introducción al Desarrollo/Clases/02 variables.py
@@ -0,0 +1,16 @@
+# Programa par auso de variables
+saludo = 'hola este es un string'
+print(saludo)
+print(type(saludo)) # el tipo es un str
+
+edad = 25
+print(edad)
+print(type(edad)) # el tipo es un int
+
+precio = 105.75
+print(precio)
+print(type(precio)) # el tipo es un float
+
+es_blanco = True #False
+print(es_blanco)
+print(type(es_blanco)) # el tipo es un bool
